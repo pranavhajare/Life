f8dbe18172e6e478bceee796dc91e99812fcdbe1
pranavhajare <hajare.pranav@gmail.com>
2024-04-23 00:19:48 +0530

    create the cells

--- src/Game.js
+++ src/Game.js
@@ -5,9 +5,90 @@ const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
-class Game extends React.Component {
-    
+class Cell extends React.Component {
   render() {
+    const { x, y } = this.props;
+    return (
+      <div
+        className="Cell"
+        style={{
+          left: `${CELL_SIZE * x + 1}px`,
+          top: `${CELL_SIZE * y + 1}px`,
+          width: `${CELL_SIZE - 1}px`,
+          height: `${CELL_SIZE - 1}px`,
+        }}
+      />
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor() {
+    super();
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
+    this.board = this.makeEmptyBoard();
+  }
+
+  state = {
+    cells: [],
+  };
+
+  //create an empty board
+  makeEmptyBoard = () => {
+    let board = [];
+    for (let y = 0; y < this.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++) {
+        board[y][x] = false;
+      }
+    }
+    return board;
+  };
+
+  //create cells from this.board
+  makeCells = () => {
+    let cells = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[y][x]) {
+          cells.push({ x, y });
+        }
+      }
+    }
+    return cells;
+  };
+
+  //to calculate the position of the board element
+  getElementOffset = () => {
+    const rect = this.boardRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: rect.left + window.scrollX - doc.clientLeft, //The read-only scrollX property of the Window interface returns the number of pixels that the document is currently scrolled horizontally.
+
+      y: rect.top + window.scrollY - doc.clientTop, //The read-only scrollY property of the Window interface returns the number of pixels that the document is currently scrolled vertically.
+    };
+  };
+
+  // handleClick willl retrieve the click position, then convert it to relative position and calculate the cols and rows of the cell being clicked. THEN the cell state is reverted.
+  handleClick = (event) => {
+    const elemOffset = this.getElementOffset();
+    const offsetX = event.clientX - elemOffset.x;
+    const offsetY = event.clientY - elemOffset.y;
+
+    const x = Math.floor(offsetX / CELL_SIZE);
+    const y = Math.floor(offsetY / CELL_SIZE);
+
+    if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+      this.board[y][x] = !this.board[y][x];
+    }
+
+    this.setState({ cells: this.makeCells() });
+  };
+
+  render() {
+    const { cells } = this.state;
     return (
       <div>
         {" "}
@@ -18,7 +99,15 @@ class Game extends React.Component {
             height: HEIGHT,
             backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
           }}
-        ></div>{" "}
+          onClick={this.handleClick}
+          ref={(n) => {
+            this.boardRef = n;
+          }}
+        >
+          {cells.map((cell) => (
+            <Cell x={cell.x + 1} y={cell.y} key={`${cell.x},${cell.y}`} />
+          ))}
+        </div>{" "}
       </div>
     );
   }
